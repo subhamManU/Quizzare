02d49f130c985693123c8975aa97e7eb4aadf7f2
deboGunner <033debo033@gmail.com>
2017-03-24 22:21:02 +0530

    Registration updated with email details

--- finalEntry.php
+++ finalEntry.php
@@ -9,12 +9,14 @@
       $dept=array();
       $roll=array();
       $year=array();
+      $email=array();
       $allow1=$allow2=$allow3=1;
       for($i=0;$i<3;$i++){
         $name[]=$_POST['name'.$i];
         $dept[]=$_POST['dept'.$i];
         $year[]=$_POST['year'.$i];
         $roll[]=$_POST['roll'.$i];
+        $email[]=$_POST['email'.$i];
       }
 
       $memcheck1="SELECT COUNT(*) as cc1 FROM table1 WHERE (table1.dept1='$dept[0]' and table1.year1='$year[0]' and table1.roll1='$roll[0]')or(table1.dept2='$dept[0]' and table1.year2='$year[0]' and table1.roll2='$roll[0]')or(table1.dept3='$dept[0]' and table1.year3='$year[0]' and table1.roll3='$roll[0]')";
@@ -44,11 +46,11 @@
       if($allow1!=0 && $allow2!=0 && $allow3!=0)
       {
 
-        $sql = $connect->prepare("INSERT INTO table1(team,name1,dept1,year1,roll1,
-        name2,dept2,year2,roll2,name3,dept3,year3,roll3)
-        VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)");
+        $sql = $connect->prepare("INSERT INTO table1(team,name1,dept1,year1,roll1,email1,
+        name2,dept2,year2,roll2,email2,name3,dept3,year3,roll3,email3)
+        VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 
-        $sql->bind_param("sssssssssssss",$team,$name[0],$dept[0],$year[0],$roll[0],$name[1],$dept[1],$year[1],$roll[1],$name[2],$dept[2],$year[2],$roll[2]);
+        $sql->bind_param("ssssssssssssssss",$team,$name[0],$dept[0],$year[0],$roll[0],$email[0],$name[1],$dept[1],$year[1],$roll[1],$email[1],$name[2],$dept[2],$year[2],$roll[2],$email[2]);
         $sql->execute();
 
         echo "success";
